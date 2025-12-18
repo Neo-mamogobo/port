972c995d2300e1082467aa9539fbf2c2b91c6345
Neo-mamogobo <dipinamamogobo@gmail.com>
2025-12-18 13:58:08 +0200

    Add files via upload

---
+++ port.js
@@ -0,0 +1,23 @@
+document.getElementById(`home`).addEventListener(`click`, (e) => {e.preventDefault();
+    document.getElementById(`homesection`).scrollIntoView({behavior: `smooth`})
+})
+
+document.getElementById(`about`).addEventListener(`click`, (e) => {e.preventDefault();
+    document.getElementById(`aboutsection`).scrollIntoView({behavior: `smooth`})
+})
+
+document.getElementById(`skill`).addEventListener(`click`, (e) => {e.preventDefault();
+    document.getElementById(`skillsection`).scrollIntoView({behavior: `smooth`})
+})
+
+document.getElementById(`experiences`).addEventListener(`click`, (e) => {e.preventDefault();
+    document.getElementById(`experincesection`).scrollIntoView({behavior: `smooth`})
+})
+
+document.getElementById(`hobbies`).addEventListener(`click`, (e) => {e.preventDefault();
+    document.getElementById(`hobbiessection`).scrollIntoView({behavior: `smooth`})
+})
+
+document.getElementById(`contact`).addEventListener(`click`, (e) => {e.preventDefault();
+    document.getElementById(`contactsection`).scrollIntoView({behavior: `smooth`})
+})
